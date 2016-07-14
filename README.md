2cc36d611e855f1fd1eaa62d24d425e24b21a6c0
Niklas Jerva <niklas.jerva@gmail.com>
2016-07-14 23:42:04 +0300

    Inital release

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sonera router check
+
+This script prints out the power and dB values of incoming and outgoing channels from a Cisco EPC3925 modem's front page whether it's in Router mode or in Bridged mode.
+
+This was done because I was having problems with my internet while I was with Sonera and had their cable connected.
+I had one incoming channel going completely dead on my end and in Sonera's system everything was showing up good. With this I was able to monitor what my modem was doing.
+While the one channel was down my speed went from 100mb/s down to 10mb/s down while my upload was ok.
+
+Stuff happens and it was fixed with a firmware update but IMO the older model EPC3825 was better.
---
+++ router_channel_status.py
@@ -0,0 +1,74 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def BrigdedMode():
+    data_pwr = []
+    data_snr = []
+    data_up = []
+    i = 0
+    j = 0
+    soup = BeautifulSoup(bridged.content, 'html5lib')
+    table = soup.find('table', {'summary': 'Downstream Channels'})
+    cols_pwr = table.findAll('td', {'headers': 'ch_pwr'})
+    cols_snr = table.findAll('td', {'headers': 'ch_snr'})
+
+    table = soup.find('table', {'summary': 'Upstream Channels'})
+    cols_up_pwr = table.findAll('td', {'headers': 'up_pwr'})
+
+    for power in cols_pwr:
+        data_pwr.append(power.text[1:-11])
+    for snr in cols_snr:
+        data_snr.append(snr.text[:-9])
+    for up_pwr in cols_up_pwr:
+        data_up.append(up_pwr.text[:-11])
+    for pwr, snr in zip(data_pwr, data_snr):
+        i += 1
+        print('Channel ' + str(i) + ': ' +
+              str(pwr) + 'dBmV, ' + str(snr) + 'dB')
+    print('')
+    print('Upstream Channel data:')
+    for up_data in data_up:
+        j += 1
+        print('Channel ' + str(j) + ': ' + str(up_data) + 'dB')
+
+
+def RouterMode():
+    data_pwr = []
+    data_snr = []
+    data_up = []
+    i = 0
+    j = 0
+    routed = requests.get('http://192.168.0.1/Docsis_system.asp')
+    soup = BeautifulSoup(routed.content, 'html5lib')
+    table = soup.find('table', {'summary': 'Downstream Channels'})
+    cols_pwr = table.findAll('td', {'headers': 'ch_pwr'})
+    cols_snr = table.findAll('td', {'headers': 'ch_snr'})
+
+    table = soup.find('table', {'summary': 'Upstream Channels'})
+    cols_up_pwr = table.findAll('td', {'headers': 'up_pwr'})
+
+    for power in cols_pwr:
+        data_pwr.append(power.text[1:-11])
+    for snr in cols_snr:
+        data_snr.append(snr.text[:-9])
+    for up_pwr in cols_up_pwr:
+        data_up.append(up_pwr.text[:-11])
+
+    print('Downstream Channel data:')
+    for pwr, snr in zip(data_pwr, data_snr):
+        i += 1
+        print('Channel ' + str(i) + ': ' +
+              str(pwr) + 'dBmV, ' + str(snr) + 'dB')
+    print('')
+    print('Upstream Channel data:')
+    for up_data in data_up:
+        j += 1
+        print('Channel ' + str(j) + ': ' + str(up_data) + 'dB')
+
+bridged = requests.get('http://192.168.100.1/Docsis_system.asp')
+if __name__ == '__main__':
+    if bridged.status_code != 200:
+        RouterMode()
+    else:
+        BrigdedMode()
